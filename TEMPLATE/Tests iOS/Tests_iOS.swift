cc2874b7d5c2cc6ab8e5e8d6030ca1905c3723f3
Gustavo Wide <gustavowide@MacBook-Pro-de-Gustavo.local>
2022-08-14 15:11:10 -0300

    added snapshot tests

--- TEMPLATE/Tests iOS/Tests_iOS.swift
+++ TEMPLATE/Tests iOS/Tests_iOS.swift
@@ -7,9 +7,30 @@
 
 import XCTest
 import Combine
+import SnapshotTesting
+import SwiftUI
 
 
 class Tests_iOS: XCTestCase {
+    
+    override func setUp() {
+        TEMPLATEApp.container = MockContainerFactory.build()
+    }
+    
+    func testHomeScreen() {
+        let vm = HomeViewModel()
+        let view = HomeView(vm: vm)
+        let expectation = XCTestExpectation(description: "loaded weather")
+        
+        vm.refresh
+            .delay(for: 1, scheduler: RunLoop.main)
+            .sink { _ in expectation.fulfill() }
+            .store(in: &cancellables)
+        
+        vm.refresh.send()
+        wait(for: [expectation], timeout: 2)
+        assertSnapshot(matching: view.toVC(), as: .image(on: .iPhoneX, precision: 0.95))
+    }
     
     func testWeatherDetail() throws {
         let expectation = self.expectation(description: "decode weather")
@@ -27,4 +48,14 @@ class Tests_iOS: XCTestCase {
     }
     
     var cancellables: Set<AnyCancellable> = []
+}
+
+extension View {
+    func toVC() -> UIViewController {
+        return UIHostingController(rootView: self)
+    }
+    
+    func testingView() -> UIView {
+        return UIHostingController(rootView: self).view
+    }
 }
